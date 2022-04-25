60f065da3b87829401b5a9e1ce25a69970b1f8f4
arisa <arisa@LAPTOP-08B0VL0V>
2022-04-25 09:39:53 -0500

    Agregar archivos de proyecto.

---
+++ Repaso 1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repase_segundo_periodo
+{
+    public partial class inico_form : Form
+    {
+        public inico_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
